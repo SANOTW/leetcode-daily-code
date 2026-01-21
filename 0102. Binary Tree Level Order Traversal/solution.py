9ed06b3f55f130d441bf678b422d31aaa92cf533
SANOTW <37615945+SANOTW@users.noreply.github.com>
2026-01-21 23:14:29 +0530

    feat(102): add solution `Solution.levelOrder`

---
+++ 0102. Binary Tree Level Order Traversal/solution.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+    
+        result = []
+
+        queue = [root]
+        front = 0
+
+        while front < len(queue):
+            level = []
+            level_size = len(queue) - front
+
+            for _ in range(level_size):
+                node = queue[front]
+                front += 1
+
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+        
+        return result
