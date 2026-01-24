9dcb10f6858124346bb3a277243db52091d5c8df
SANOTW <37615945+SANOTW@users.noreply.github.com>
2026-01-24 10:35:08 +0530

    chore(20): document alternat stack approach

--- 0020. Valid Parentheses/solution.py
+++ 0020. Valid Parentheses/solution.py
@@ -24,3 +24,28 @@ class Solution:
                 stack.pop()            
 
         return not stack
+
+'''alternate solution
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dictionary = {
+            ")" : "(",
+            "}" : "{",
+            "]" : "["
+        }
+
+        stack = []
+
+        for i in s:
+            if i in dictionary.values():
+                stack.append(i)
+            
+            elif stack and dictionary[i] == stack[-1]:
+                stack.pop()
+            
+            else:
+                return False
+
+        return not stack
+'''
